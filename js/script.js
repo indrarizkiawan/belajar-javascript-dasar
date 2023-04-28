b7b8df00907f55fe401cb77e750943de7b56a28e
Indra Rizkiawan <indra.rizkiawan@gmail.com>
2023-04-29 01:00:42 +0700

    menambah tipe data number

--- js/script.js
+++ js/script.js
@@ -4,3 +4,22 @@ document.writeln("Hello World");
 ini adalah komentar
 banyak baris
 */
+document.writeln("<hr>");
+// Tipe data number
+document.writeln("Tipe Data Number :");
+document.writeln(100);
+
+document.writeln("<br>");
+document.writeln("Tipe Data Number :");
+document.writeln(100.123);
+document.writeln("<hr>");
+
+// Tipe data number notation
+document.writeln("Hexadecimal 0xff : ");
+document.writeln(0xff);
+document.writeln("<br>");
+document.writeln("Binary 0b101010 : ");
+document.writeln(0b101010);
+document.writeln("<br>");
+document.writeln("Octal 0o10 : ");
+document.writeln(0o10);
